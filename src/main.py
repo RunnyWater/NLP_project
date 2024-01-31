861dce2aa0c13196a44c7838b7ec64bb65993f2c
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-01-31 15:05:18 +0100

    deleteed unnecessary functions

--- src/main.py
+++ src/main.py
@@ -10,27 +10,5 @@ def main():
     processor = messageProcessor()
 
 
-
-def decode_text(encoded_text, word_to_index):
-    # Replace each integer in the encoded text with its corresponding word
-    decoded_text = [word_to_index.get(index, 'UNK') for index in encoded_text]
-
-    return ' '.join(decoded_text)
-
-
-def encode_text(text):
-    # Split the text into words
-    words = text.split()
-
-    # Create a dictionary to map each word to a unique integer
-    word_to_index = {}
-    for i, word in enumerate(words):
-        word_to_index[word] = i
-
-    # Replace each word in the text with its corresponding integer
-    encoded_text = [word_to_index[word] for word in words]
-
-    return encoded_text, word_to_index
-
 if __name__ == '__main__':
     main()
