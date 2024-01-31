064b701b58c1f6dd8408b217d6b9ae392533aed1
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-01-31 03:06:46 +0100

    Full project commit

--- src/main.py
+++ src/main.py
@@ -5,29 +5,10 @@ from database.database_manager import DatabaseManager
 
 def main():
     dir = 'txt_subs' # Directory that contains txt files that needed to be converted to csv
-    # db = 'database.sql'
-    # db_manager = DatabaseManager()
-    # processor = messageProcessor()
-    # message = processor.filter_message_to_list("Hello, how are you? I need to make a math problem. Can you help me with it? I'm really struggling with my math. So you will be a great help")
-    
-    # message = np.array(message).tobytes()
-    # print(message)
-    # decoded_text = list(np.frombuffer(message, dtype=np.int64))
-    # print(decoded_text)
+    db = 'database.sql'
+    db_manager = DatabaseManager()
+    processor = messageProcessor()
 
-
-    encoded_text, word_to_index = encode_text("Do you remember, we talked about this and that few months ago?")
-    print(word_to_index)
-    # Decode the text
-    decoded_text = decode_text(encoded_text, word_to_index)
-
-    print(decoded_text)
-
-
-    # db_manager.clearing_table('database.sql', )
-    # processor.topic_modeling()
-    # db_manager.csv_files_to_sql()
-    # processor.csv_to_sql_handler()
 
 
 def decode_text(encoded_text, word_to_index):
